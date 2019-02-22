9b68bebd07a91ed6f62b9ffac4305c633c54d91a
neha912 <nehanakrani004@gmail.com>
2019-02-23 01:34:40 +0530

    First commit for rails admin gem install

---
+++ db/migrate/20190222194658_create_user_contacts.rb
@@ -0,0 +1,12 @@
+class CreateUserContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_contacts do |t|
+      t.string :name
+      t.string :email
+      t.text :address
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
